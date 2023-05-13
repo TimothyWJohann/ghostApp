3e1605ea6621123d447f49f4edca6995c515bb1b
HamSandwichWithHam <johann@roanoke.edu>
2023-05-13 08:43:52 -0400

    Landing and active post pages integrated

--- App.js
+++ App.js
@@ -1,29 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, FlatList, Image, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, FlatList, Image, ScrollView, Pressable } from 'react-native';
 import React, { useState, Component, useEffect } from 'react';
 import { WebView } from 'react-native-webview';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function ActivePost({ navigation }) {
-  const [pageHTML, setPageHTML] = useState('');
+function ActivePost({ route, navigation }) {
+  /*const [pageHTML, setPageHTML] = useState('');
   const [topImage, setTopImage] = useState('');
-  const [topImageCaption, setTopImageCaption] = useState('');
+  const [topImageCaption, setTopImageCaption] = useState('');*/
+  //console.log("IN ACTIVE POST");
+  const postToDisplay = route.params.postToDisplay;
+  //console.log("POSTTODISPLAY", postToDisplay);
+  const topImage = postToDisplay["image"];
+  const topImageCaption = postToDisplay["caption"];
+  const pageHTML = postToDisplay["html"];
 
-  fetch("https://cathy.sarisky.link/ghost/api/content/posts/?key=e40b1d2e5d73dbfce53c612c7a&limit=1")
-    .then(result => result.json())
-    .then(json => {
-      postData = json["posts"][0]["html"];
-      imageAddress = json["posts"][0]["feature_image"];
-      imageCaption = json["posts"][0]["feature_image_caption"];
-      setPageHTML(postData);
-      setTopImage(imageAddress);
-      setTopImageCaption(imageCaption);
-      //console.log(json);
-    });
+  /* fetch("https://cathy.sarisky.link/ghost/api/content/posts/?key=e40b1d2e5d73dbfce53c612c7a&limit=1")
+     .then(result => result.json())
+     .then(json => {
+       postData = json["posts"][0]["html"];
+       let imageAddress = json["posts"][0]["feature_image"];
+       let imageCaption = json["posts"][0]["feature_image_caption"];
+       setPageHTML(postData);
+       setTopImage(imageAddress);
+       setTopImageCaption(imageCaption);
+       //console.log(json);
+     });*/
 
   return (
-    <View style={styles.container}>
+    <View style={styles.activepost}>
       <WebView style={styles.webView}
         originWhitelist={['*']}
         source={{
@@ -46,12 +52,12 @@ function LandingScreenSeparator() {
   )
 }
 
-function LandingScreen(navigation) {
-  const [numberThumbnails, setNumberThumbnails] = useState(6);
+function LandingScreen({ navigation }) {
+  const [numberThumbnails, setNumberThumbnails] = useState(8);
   const [thumbnailData, setThumbnailData] = useState({});
   const [newPostsReady, setNewPostsReady] = useState('true');
   useEffect(() => {
-    fetch(`https://cathy.sarisky.link/ghost/api/content/posts/?key=e40b1d2e5d73dbfce53c612c7a&limit=${numberThumbnails}&fields=id,title,feature_image,custom_excerpt`)
+    fetch(`https://cathy.sarisky.link/ghost/api/content/posts/?key=e40b1d2e5d73dbfce53c612c7a&limit=${numberThumbnails}&fields=id,title,feature_image,excerpt,feature_image_caption,html`)
       .then(result => result.json())
       .then(json => {
         let tempThumbnailData = thumbnailData;
@@ -59,31 +65,39 @@ function LandingScreen(navigation) {
           let thumbnailId = json["posts"][thumbnail]["id"];
           let thumbnailImageAddress = json["posts"][thumbnail]["feature_image"];
           let thumbnailTitle = json["posts"][thumbnail]["title"];
-          tempThumbnailData[thumbnailId] = { image: thumbnailImageAddress, title: thumbnailTitle };
+          let thumbnailExcerpt = json["posts"][thumbnail]["excerpt"];
+          let thumbnailHTML = json["posts"][thumbnail]["html"];
+          let thumbnailCaption = json["posts"][thumbnail]["feature_image_caption"];
+          tempThumbnailData[thumbnailId] = {
+            image: thumbnailImageAddress,
+            title: thumbnailTitle,
+            excerpt: thumbnailExcerpt,
+            html: thumbnailHTML,
+            caption: thumbnailCaption
+          };
+
         };
         setThumbnailData(tempThumbnailData);
         setNewPostsReady(!newPostsReady);
-        console.log("THUMBNAILDATA", thumbnailData["63c469bc09f2381d5f266e18"]['custom_excerpt']);
+        //console.log("THUMBNAILDATA", thumbnailData);
       })
       .catch(error => { console.log("ERROR", error) });
   }, [])
 
   return (
-    <View style={{ flex: 1 }}>
-      <FlatList
-        data={Object.keys(thumbnailData)}
-        renderItem={({ item }) =>
-          <View style={styles.landingpost}>
-            <Text style={styles.landingtitle}>{thumbnailData[item]['title']}</Text>
-            <Text style={styles.landingexcerpt}>{thumbnailData[item]['excerpt']}</Text>
-            <Image style={styles.container} source={{ uri: `${thumbnailData[item]['image']}` }} />
-          </View>
-        }
-        keyExtractor={item => item}
-        extraData={newPostsReady}
-        ItemSeparatorComponent={LandingScreenSeparator}
-      />
-    </View>
+    <FlatList
+      data={Object.keys(thumbnailData)}
+      renderItem={({ item }) =>
+        <Pressable style={styles.landingpost} onPress={() => { navigation.navigate('ActivePost', { postToDisplay: thumbnailData[item] }) }}>
+          <Text style={styles.landingtitle}>{thumbnailData[item]['title']}</Text>
+          <Text style={styles.landingexcerpt}>{thumbnailData[item]['excerpt']}</Text>
+          <Image style={styles.container} source={{ uri: `${thumbnailData[item]['image']}` }} />
+        </Pressable>
+      }
+      keyExtractor={item => item}
+      extraData={newPostsReady}
+      ItemSeparatorComponent={LandingScreenSeparator}
+    />
   )
 }
 
@@ -120,8 +134,16 @@ const styles = StyleSheet.create({
     margin: 15,
   },
   landingtitle: {
-    flex: 1,
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  landingexcerpt: {
+    margin: 10,
+  },
+  activepost: {
+    width: '100%',
+    resizeMode: 'contain',
+    flex: 1,
+    minHeight: 100,
   }
 });
