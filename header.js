2cb3de5e16877fa3421a7a20837cecbab0e00c7b
HamSandwichWithHam <johann@roanoke.edu>
2023-05-19 09:35:23 -0400

    Pressable and separate file for header

---
+++ header.js
@@ -0,0 +1,42 @@
+import { Button, Pressable, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/Entypo';
+import { styles } from './styles';
+
+export function HeaderRight({ idOfNextPost, postIds, navigation }) {
+    if (idOfNextPost) {
+        return (
+            <Pressable onPress={() =>
+                    navigation.push('ActivePost', { idOfPostToDisplay: idOfNextPost, postIdList: postIds })}>
+                <Text style={styles.headertext}>Next Post</Text>
+            </Pressable>
+        )
+    };
+};
+
+export function HeaderLeft({ idOfPreviousPost, postIds, navigation }) {
+    if (idOfPreviousPost) {
+        return (
+            <Pressable onPress={() =>
+                navigation.push('ActivePost', { idOfPostToDisplay: idOfPreviousPost, postIdList: postIds })}  >
+                <Text style={styles.headertext}>Prev Post</Text>
+            </Pressable>
+        )
+    }
+    else {
+        return (
+            <Pressable onPress={() =>
+                navigation.navigate('Pick A Post')} >
+                <Text style={styles.headertext}>Pick A Post</Text>
+            </Pressable>
+        )
+    };
+};
+
+export function HeaderTitle({ navigation }) {
+    return (
+        <Pressable onPress={() =>
+            navigation.navigate('Pick A Post')} >
+            <Icon name="home" color="blue" backgroundColor='white' size={40} marginLeft={0} />
+        </Pressable>
+    )
+};
